4ca00e0aab0a19dd2a20888bebf91505a45af2ff
Justin de Vesine <justin@crowdfavorite.com>
2013-01-21 12:21:05 -0700

    also handle default dataload

--- testload.php
+++ testload.php
@@ -28,7 +28,7 @@
     <li>/ncbi-updates/from-client/PMC3256938kipling.xml</li>
     <li>test/xml/short-kipling.xml</li>
   </ul>
-  Data file: <input type="text" id="datafile" name="datafile" value="<?php if ($_GET['s']) { echo $_GET['s']; } else { echo '/ncbi-updates/from-client/PMC3153123kipling.xml'; } ?>"/> <input type="button" name="loaddata" id="loaddata" value="load"/>
+  Data file: <input type="text" id="datafile" name="datafile" value="<?php if (isset($_GET['s'])) { echo $_GET['s']; } else { echo '/ncbi-updates/from-client/PMC3153123kipling.xml'; } ?>"/> <input type="button" name="loaddata" id="loaddata" value="load"/>
   <br/>
   <input type="button" name="savedata" id="savedata" value="save"/>
   <script>
