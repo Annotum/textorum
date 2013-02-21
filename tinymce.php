4d55a6a0b591e64cbe83efc6a88d68db032a5ca6
Justin de Vesine <justin@crowdfavorite.com>
2013-02-21 15:03:27 -0700

    remove extra toolbar buttons

--- tinymce.php
+++ tinymce.php
@@ -53,7 +53,10 @@
       plugins: '-textorum,-jqueryinlinepopups',
       entity_encoding: 'raw',
       content_css: 'lib/textorum/tinymce/plugin.css',
-
+    theme_advanced_buttons1 : "save,fullscreen,undo,redo,help,code,visualaid,visualchars,visualblocks",
+    theme_advanced_buttons2 : "", //cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+    theme_advanced_buttons3 : "", //tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+    theme_advanced_buttons4 : "", //insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft,visualblocks",
       keep_styles: false,
       theme_advanced_resizing: true,
       theme_advanced_resize_horizontal: false,
